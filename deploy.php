4866656fce1d10e74713790a8c0b5c2e010b1d4d
rowiredu.ing@gmail.com <rowiredu.ing@gmail.com>
2024-02-15 04:42:09 +0000

    Modified workflow

--- deploy.php
+++ deploy.php
@@ -41,7 +41,7 @@ task('deploy:secrets', function () {
 // Hosts
 ///////////////////////////////////
 
-host('prod') // Name of the server
+host('agricgate.farm') // Name of the server
 ->setHostname('162.0.229.248') // Hostname or IP address
 ->set('remote_user', 'agrigcwj') // SSH user
 ->set('port', '21098')
